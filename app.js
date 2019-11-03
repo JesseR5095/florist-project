b837496b5d601882d15f36340198b9050c41f6f9
Signl Interactive <signlinteractive@Signls-Mac-Pro.local>
2019-11-02 23:10:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+
+var app = express();
+
+const server = require('http').createServer(app);
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+server.listen(port, function () {
+   console.log('Florist project running...');
+});
